212e4113b00b691be2c2a2603456de404b4835a6
Yehonatan Levi <leviyehonatan@gmail.com>
2021-11-02 17:52:43 +0200

    test component

---
+++ src/TestComponent.js
@@ -0,0 +1,5 @@
+import { Router } from "./components/Router";
+
+export const TestComponent = () => (
+    <Router />
+)
---
+++ src/components/Router.js
@@ -0,0 +1,6 @@
+
+export const Router = () => (
+    <div>
+        <h2>I am learning react</h2>
+    </div>
+)
