53158d01cd0b553575246dd928562e7ae563b56d
אורית אופיר <orit902@gmail.com>
2021-11-04 13:14:57 +0200

    booking request details with form

--- src/components/BookingRequestDetails/BookingRequestDetails.js
+++ src/components/BookingRequestDetails/BookingRequestDetails.js
@@ -1,54 +1,110 @@
 import React from "react";
 import {useState} from "react";
-import { Form, DatePicker, TimePicker, Button, Calendar } from 'antd';
-
-
+import { Form, DatePicker, TimePicker, Button, Calendar, Select } from 'antd';
+import { continueStatement } from "@babel/types";
+const { RangePicker } = DatePicker;
 
 function onPanelChange(value, mode) {
     console.log(value, mode);
   }
-//   date_room,hourStart,hourEnd
-function sendDateMeeting(){
 
-  }
-  const BookingRequestDetails = ({ user }) =>{
-    const { RangePicker } = DatePicker;
-    
-  }
-  
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const config = {
+  rules: [
+    {
+      type: 'object',
+      required: true,
+      message: 'Please select time!',
+    },
+  ],
+};
+const rangeConfig = {
+  rules: [
+    {
+      type: 'array',
+      required: true,
+      message: 'Please select time!',
+    },
+  ],
+};
 
-// const BookingRequestDetails = ({ user }) => {
-//     const [bookingRequestDetails, setBookingRequestDetails ]= useState({})
-    
 
-//     return (
-//         <div style={{
-//             width:'400px',
-//             margin:'3em auto',
-//             direction:'rtl'
-//         }}>
-//             <span>שלום </span>
-//             <span>user.userName</span>
-//             <div>ברוכים הבאים למערכת זימון החדרים של בנימין טק. למתי לשריין את החדר?</div>
-//             <div className="site-calendar-demo-card">
-//                 <Calendar fullscreen={false} onPanelChange={onPanelChange}/>
-//             </div>
-//             <div>
-//                 <div>משעה</div>
-//                 <TimePicker
-//                     minuteStep={15}
-//                     format='HH:mm'
-//                     placeholder="בחר שעה" 
-//                     value={hourStart}
-//                     onChange={(e)=>(setBookingRequestDetails({...bookingRequestDetails,fromTime: e}))}/>
-//             </div>
-//             <div>
-//                 <div>עד שעה</div>
-//                 <TimePicker minuteStep={15} format='HH:mm' placeholder="בחר שעה" />
-//             </div>
-//             <input name="ok" type="submit" value="מתאים לי בדיוק" onClick={()=>{alert("הייי")}}/>
-//         </div>
-//     );
-// }
+const BookingRequestDetails = ({ user }) => {
+    const onFinish = (fieldsValue) => {console.log(fieldsValue)}
+    const { Option } = Select;
+  return (
+    <Form
+    name="booking_request_details" {...formItemLayout}
+    onFinish={onFinish}
+    style={{
+        width:'400px',
+        margin:'3em auto',
+        direction:'rtl'
+        }}>
+        <span>שלום </span>
+        <span>user.userName</span>
+        <div>ברוכים הבאים למערכת זימון החדרים של בנימין טק. למתי לשריין את החדר?</div>
+      <Form.Item name="my_calander" label="calendar" {...config}>
+          <div>
+              <br/>
+              <br/>
+          <Calendar fullscreen={false}/>
+          </div>
+      </Form.Item>
+      <Form.Item name="from_time" label="משעה" {...config}>
+        <TimePicker
+            minuteStep={15}
+            format='HH:mm'
+            placeholder="בחר שעה" />
+      </Form.Item>
+      <Form.Item name="to_time" label="משעה" {...config}>
+        <TimePicker
+            minuteStep={15}
+            format='HH:mm'
+            placeholder="בחר שעה" />
+      </Form.Item>
+      <Form.Item name="num" label="עבור" {...config}>
+        <Select defaultValue="2" style={{ width: 80 }} bordered={false}>
+        <Option value="2">2</Option>
+        <Option value="3">3</Option>
+        <Option value="4">4</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        }}
+      >
+        <Button type="primary" htmlType="מתאים לי בדיוק">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default BookingRequestDetails
