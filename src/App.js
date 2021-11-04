6be3c6c16ef8b63a1478736f51d4cd6dec2ae6d3
chani amar <chani110161@gmail.com>
2021-11-04 10:59:33 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Router } from './components/Router';
 import { TestComponent } from './TestComponent';
+import { ConfigProvider } from 'antd';
+import he_IL from 'antd/lib/locale/he_IL';
 
 function App() {
   return (
+    <ConfigProvider locale={he_IL}>
     <div>
       <TestComponent />
     </div>
+    </ConfigProvider>
   );
 }
 
