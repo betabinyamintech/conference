04200a9ff8a680afe3cc036a17e0462977ec0775
אורית אופיר <orit902@gmail.com>
2021-11-07 09:56:02 +0200

    with mock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import './App.css';
+import TestComponet from './TestComponent';
 
 function App() {
   return (
     <div>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Login</Link>
+      <TestComponet/>
+      {/* <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link> */}
     </div>
   );
 }
--- src/context/user.js
+++ src/context/user.js
@@ -1,11 +1,14 @@
 import React, { useState, useContext } from "react"
+import {userSchema} from "./mock"
 
+const user=userSchema
 export const UserContext = React.createContext()
 
 // container to provide the context
 export const UserProvider = ({ children }) => {
+    console.log("in context")
     // this is our actual storage
-    const [userState, setUserState] = useState()
+    const [userState, setUserState] = useState(user)
     return (
         <UserContext.Provider value={[userState, setUserState]}>
             {children}
