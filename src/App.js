4d8fb5c7e0c71556ae111cb82b5ee4f9904d0a59
אורית אופיר <orit902@gmail.com>
2021-11-07 10:51:17 +0200

    111

--- src/App.js
+++ src/App.js
@@ -6,16 +6,9 @@ import TestComponet from './TestComponent';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <TestComponet/>
-      {/* <Link to="/register">Register</Link>
-      <Link to="/login">Login</Link> */}
-=======
       <Link to="/register">Register</Link><br/>
       <Link to="/login">Login</Link><br/>
-      <Link to="/bookingResponse">BookingResponse</Link>
-      
->>>>>>> 4404405639735a2d4c5f91b014d41eae12dc36ab
+      <Link to="/bookingResponse">BookingResponse</Link>  
     </div>
   );
 }
