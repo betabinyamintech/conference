e69ee2b6ca4a43f72673cad82d4a6a1cb98f82f4
Rachel <rachelperets34@gmail.com>
2021-11-10 20:35:30 +0200

    book menu

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,12 +1,13 @@
 import BookingRequestDetails from "./components/BookingRequestDetails/BookingRequestDetails";
 import React from 'react'
 import BookingAlternatives from "./components/BookingResponse/BookingAlternatives";
+import { BookingMenu } from "./components/BookingMenu";
 
 
 export const TestComponent = () => {
     return(
     <div>
-        <BookingAlternatives/>
+        <BookingMenu/>
     </div>
     )
 }
--- src/components/BookingMenu.js
+++ src/components/BookingMenu.js
@@ -1,24 +1,28 @@
-import { Menu } from 'antd';
+import { Tabs  } from 'antd';
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 export const BookingMenu = () => {
+  //כאן נשאב את כל ההזמנות של היוזר הספציפי הזה
+  const allBookings=useEffect(()=>{
 
-  const [clickedMenu, setclickedMenu] = useState("nextBooking")
+  },[])
 
-  const Clicked = () => {
-    // setclickedMenu( e.key );
-    alert("clicked") 
-  };
+    const { TabPane } = Tabs; 
+  
+    
   return (
-    <Menu  mode="horizontal" >
-      <Menu.Item key="history" onClick={() => { Clicked() }}>
-        הסטוריית הזמנות
-      </Menu.Item>
-      <Menu.Item key="nextBooking" onClick={() => { Clicked() }} >
-        חדרים מוזמנים
-      </Menu.Item>
-    </Menu>
+    
+      <Tabs defaultActiveKey="1" >
+        <TabPane tab="הסטוריית הזמנות" key="1">
+          {book}
+        </TabPane>
+        <TabPane tab="חדרים מוזמנים" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        
+      </Tabs>
+    
 
   )
 }
