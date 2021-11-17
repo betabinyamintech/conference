cb0b1cc3af4b58051902c4b54f9e4bffaddfafd9
Rachel <rachelperets34@gmail.com>
2021-11-17 19:22:36 +0200

    user

--- src/context/user.js
+++ src/context/user.js
@@ -1,33 +1,28 @@
 import React, { useState, useEffect } from "react"
-import { useNavigate } from "react-router"
 import { getUserDetails } from "../actions/auth"
 export const UserContext = React.createContext()
 
 // container to provide the context
 export const UserProvider = ({ children }) => {
     // this is our actual storage
-    const [userState, setUserState] = useState()
+    const [userState, setUserState] = useState({})
 
-    async function loginToken() {
-        console.log('loginToken')
-        // we call the server for details if we have token
-        try {
-            const userDetails = await getUserDetails()
-            setUserState(userDetails)
-            console.log('userState', userDetails)
-        } catch (error) {
-            console.log("an error has occured getting user details: ", error);
-        }
-    }
     // use effect will be run once with [] empty dependencies
     useEffect(() => {
         // async function seperate for useEffect
-        console.log('usercontext startup with token: ', localStorage.getItem('token'))
-        if (localStorage.getItem('token'))
-            loginToken()
+        async function getIt() {
+            // we call the server for details if we have token
+            try {
+                const userDetails = await getUserDetails()
+                setUserState(userDetails)
+            } catch (error) {
+                console.log("an error has occured getting user details: ", error);
+            }
+        }
+        getIt()
     }, [])
     return (
-        <UserContext.Provider value={{ userState, setUserState, loginToken }}>
+        <UserContext.Provider value={{ userState, setUserState }}>
             {children}
         </UserContext.Provider>
     )
