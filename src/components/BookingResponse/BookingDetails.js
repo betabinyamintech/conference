4f4dea08d597b3a652c1143d02835416a436129e
orit-polacheck <orit902@gmail.com>
2021-11-18 16:11:35 +0200

    all functions work

--- src/components/BookingResponse/BookingDetails.js
+++ src/components/BookingResponse/BookingDetails.js
@@ -7,16 +7,9 @@ var moment = require('moment'); // require
 
 const BookingDetails = ({ book, details, setBookingRequestResponse }) => {
   console.log("in the modal: ", details)
-  let { date, startTime, endTime, roomDetails } = details
-  const { userState } = useContext(UserContext)
-  const userId = userState._id
-  console.log(
-    "startTime: ", startTime,
-    " startTimeMoment: ",moment.unix(startTime),
-    " startTimeMomentAndFormat: ",moment.unix(startTime).format('HHmm'),
-  )
-  let stringDate = moment(date, 'YYYYMMDD').format('l')
-  let day = moment(date, 'YYYYMMDD').format('dddd')
+  let { meetingDate, startTime, endTime, roomDetails } = details
+  let stringDate = moment(meetingDate, 'YYYYMMDD').format('l')
+  let day = moment(meetingDate, 'YYYYMMDD').format('dddd')
   let fromTime=moment.unix(startTime).format('HHmm')
   let toTime=moment.unix(endTime).format('HHmm')
   let toTimeString=toTime.slice(0, 2) + ":" + toTime.slice(2);
