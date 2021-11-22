a240f51e1f583f0da628e0b29ebeadb137583d0f
Avishy1980 <67895686+Avishy1980@users.noreply.github.com>
2021-11-22 12:06:29 +0200

    fix css file commit

--- src/components/UserProfile/ProfileHeader.js
+++ src/components/UserProfile/ProfileHeader.js
@@ -1,10 +1,10 @@
-
 import React, { useContext } from "react";
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import Bt_logo from '../../img/binyamintech-logo.png';
 import { UserContext } from "../../context/user";
-import '../../App.css'
+import '../../App.css';
+import './ProfileHeader.css';
 
 export default function ProfileHeader() {
 
@@ -14,32 +14,25 @@ export default function ProfileHeader() {
 
 
         <div>
-
-           
-                <div className="profileHeader">
-                    {/* style={{ display: 'flex', backgroundColor: 'white', justifyContent: 'space-between' }} */}
-                    <div className="img">
-                        <img alt="example" src={Bt_logo} style={{ margin: 8 }} />
-                    </div>
-                    {(userState ?
-                    <div className="details" style={{ display: 'flex' }}>
-                        <div className="fullName" style={{ marginRight: 10 + 'px' }}>
-                            <h4>{"שלום" + ', ' + userState.name}</h4>
+            <div className="profileHeader">
+                <div className="img">
+                    <img alt="example" src={Bt_logo} />
+                </div>
+                {(userState ?
+                    <div className="details">
+                        <div className="fullName">
+                            <h4>{"שלום,"} {userState.name}</h4>
                             <div className="bitCoins">
-                                <h6>יתרה 100 אסימונים</h6>
+                                <h6>יתרתך 100 אסימונים</h6>
                             </div>
                         </div>
-                        <div className="avatar" style={{ margin: 8 }}>
-                            <Avatar icon={<UserOutlined />} />
+                        <div className="avatar">
+                            <Avatar icon={<UserOutlined/>} />
                         </div>
 
-                    </div>: <div>שלום אורח</div>)}
-
-                </div> 
-
+                    </div> : <div>שלום אורח</div>
+                )}
+            </div>
         </div>
-
     )
 }
-
-// export default ProfileHeader;
