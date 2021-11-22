2a07f48819f107985a32af0e4f1f2fdaafe7e6cc
orit-polacheck <orit902@gmail.com>
2021-11-22 09:48:14 +0200

    home with modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import { Button } from "antd";
 import React, { useState, useContext } from "react";
 import { PlusOutlined, LeftOutlined, RightOutlined, BoldOutlined } from '@ant-design/icons';
-import { Link } from "react-router-dom";
-import { Carousel } from 'antd';
+import { Link, useNavigate } from "react-router-dom";
+import { Carousel, Tabs, Modal } from 'antd';
 import english from '../img/english.jpg';
 import wordPress from '../img/wordPress.jpg';
 import ProfileHeader from "./UserProfile/ProfileHeader";
 import '../components/Home.css';
 import { UserContext } from "../context/user";
+import { render } from "@testing-library/react";
+
+
+
+const { TabPane } = Tabs;
 
 function onChange(a, b, c) {
 }
@@ -34,30 +39,61 @@ const middleDiv = {
   lineHeight: '80px',
 
 }
+const ModalLogin = () => {
+  
+return <Modal centered visible="true" maskClosable >
+        <Tabs defaultActiveKey="1"  >
+            <TabPane tab="כניסה עם sms" key="1"  >
+                
+            </TabPane>
+            <TabPane tab="כניסה עם סיסמא" key="2"  >
+            
+            </TabPane>
+        </Tabs>
+      </Modal>
+}
 
 
 
 const Home = () => {
   const {userState} = useContext(UserContext)
+  const navigate = useNavigate()
+  const [showModalLogin, setShowModalLogin] = useState(false)
+  const NewBookingButton= ()=>{
+    console.log("in newBookingButton", showModalLogin)
+    if(userState){
+      navigate("/bookrequest")
+    }
+    else{
+      setShowModalLogin(true)
+      console.log("after ", showModalLogin)
+    }
+    
+  }
   return (
     <>
-    
       {userState&&<ProfileHeader/>}
-      {!userState&&<>
+      {/* {!userState&&<>
       <Link to="/register"><button >לקוח חדש</button></Link>
       <button>לקוח קיים</button>
       </>}
+      {!userState&&<>
+      <Link to="/register"><button > לקוח חדש</button></Link>
+      <button onClick={()=>(setShowModalLogin(true))}> לקוח קיים</button>
+      </>} */}
+      {showModalLogin&&<ModalLogin/>}
       <div style={{ height: '20px' }}></div>
       <div>
         <div style={headDiv}>
-          <Link to="/bookrequest">
-            <Button type="primary" style={{ background: '#00AAAF' }} shape="round" icon={<PlusOutlined />} size={"large"}>
+            <Button type="primary" style={{ background: '#00AAAF' }}
+              shape="round" icon={<PlusOutlined />} size={"large"}
+              onClick={()=>{NewBookingButton()}}>
               פגישה חדשה
-            </Button></Link>
+            </Button>
         </div>
         <div >
           <div >
-            {/* <p style={text}>כדאי לדעת</p> */}
+            <p>כדאי לדעת</p>
             <Carousel afterChange={onChange} arrows prevArrow={<LeftOutlined />} nextArrow={<RightOutlined />}>
               <div>
                 <h3 style={contentStyle}>
