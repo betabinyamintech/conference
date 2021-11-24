474b5fceea3b2b4ab13b1cae406fc2384da3c3fe
Yehonatan Levi <leviyehonatan@gmail.com>
2021-11-24 14:28:01 +0200

    /

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ console.log('location', location)
 const location = window.location
 const isLocalhost = location.hostname === "localhost" || location.hostname === "127.0.0.1"
 const localhostEndpoint = 'http://localhost:4000'
-const productionEndpoint = 'https://conference-rooms.herokuapp.com/'
+const productionEndpoint = 'https://conference-rooms.herokuapp.com'
 export const baseUrl = isLocalhost ? localhostEndpoint : productionEndpoint
 
